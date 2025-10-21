3b2f3c47304ad990df3ed27bc7a086aa430506b6
Junior27VQ <favq1999@outlook.com>
2025-10-20 19:35:15 -0500

    funciones implementadas

--- cuentas.js
+++ cuentas.js
@@ -16,6 +16,14 @@ mostrarCuentas=function(){
         Columnas: NUMERO CUENTA, NOMBRE, SALDO
         En la columna NOMBRE concatenar el nombre y el apellido
     */
+    let cmpTabla=document.getElementById("mostrarTabla");
+    let tabla="<table>";
+    for(let i=0; i<cuentas.length; i++){
+        tabla+="<tr><td>"+cuentas[i].numeroCuenta+"</td><td>"+cuentas[i].cedula+"</td><td>"
+        +cuentas[i].nombre+"</td><td>"+cuentas[i].apellido+"</td><td>"+cuentas[i].saldo+"</td></tr>";
+    }
+    tabla+="</table>";
+    cmpTabla.innerHTML=tabla;
 }
 
 /*
@@ -23,7 +31,16 @@ mostrarCuentas=function(){
     si existe retorna el objeto cuenta, caso contrario retorna null. 
 */
 buscarCuenta=function(numeroCuenta){
-
+    let persona;
+    let personaEncontrada=null;
+    for(let i=0; i<cuentas.length; i++){
+        persona=cuentas[i];
+        if (persona.numeroCuenta == numeroCuenta.numeroCuenta) {
+            personaEncontrada = persona;
+            break;
+        }
+    }
+    return personaEncontrada;
 }
 
 /*
@@ -33,6 +50,13 @@ buscarCuenta=function(numeroCuenta){
 agregarCuenta=function(cuenta){
     //Si ya existe mostrar un alert CUENTA EXISTENTE
     //Si se agrega, mostrar un alert CUENTA AGREGADA
+    let persona=buscarCuenta(cuenta);
+    if(persona==null){
+        cuentas.push(cuenta);
+        alert("CUENTA AGREGADA");
+    }else{
+        alert("CUENTA EXISTENTE");
+    }
 }
 
 agregar=function(){
@@ -40,4 +64,17 @@ agregar=function(){
     //Crea un objeto cuenta y agrega los atributos con los valores de las cajas respectivas
     //Invoca a agregarCuenta
     //Invoca a mostrarCuentas
+    let nuevaCuenta={};
+    
+    let numeroCuenta=recuperarTexto("txtNumero");
+    let cedula=recuperarTexto("txtCedula");
+    let nombre=recuperarTexto("txtNombre");
+    let apellido=recuperarTexto("txtApellido");
+    nuevaCuenta.numeroCuenta=numeroCuenta;
+    nuevaCuenta.cedula=cedula;
+    nuevaCuenta.nombre=nombre;
+    nuevaCuenta.apellido=apellido;
+    nuevaCuenta.saldo=0.0;
+    agregarCuenta(nuevaCuenta);
+    mostrarCuentas();
 }
